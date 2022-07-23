c7d6620ee90714647e2801e722194e65cd6535a9
naoto <soe3.nao10@gmail.com>
2022-07-23 21:59:43 +0900

    管理者ユーザーの作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create!(name:  "管理者",
+  kana: "カンリシャ",
+  tel: "09011112222",
+  email: "admin@com",
+  password:  "111aaa",
+  password_confirmation: "111aaa",
+  admin: true)
