46c575e5ae3d72ec7a9058bf5b2bc213a950ca6f
naoto <soe3.nao10@gmail.com>
2022-07-28 20:01:30 +0900

    注文画面の作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'items#index'
-  resources :items, only: [:index, :show]
+  resources :items, only: [:index, :show] do
+    resources :orders, only: [:index]
+  end
     namespace :admin do
       resources :items, only: [:index, :new, :create, :edit, :update]
     end
